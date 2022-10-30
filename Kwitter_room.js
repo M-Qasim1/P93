981cfd12070d44e570fd4f75205c50f53cbbdf99
M-Qasim1 <106485573+M-Qasim1@users.noreply.github.com>
2022-10-30 17:47:10 +0000

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCqoP9HZNfiuEg-3GUUUmS6ovbdMcK_kVw",
+    authDomain: "kwitterproject-b1384.firebaseapp.com",
+    projectId: "kwitterproject-b1384",
+    storageBucket: "kwitterproject-b1384.appspot.com",
+    messagingSenderId: "199515956176",
+    appId: "1:199515956176:web:d37f08c0a58e433b190fd5"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
